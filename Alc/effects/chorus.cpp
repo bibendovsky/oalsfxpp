4fec8543540b8955623d3cb9a3fffab1dfecc230
bibendovsky <bibendovsky@hotmail.com>
2017-08-31 17:01:06 +0300

    Revise chorus effect

--- Alc/effects/chorus.cpp
+++ Alc/effects/chorus.cpp
@@ -37,13 +37,13 @@ public:
     ChorusEffect()
         :
         IEffect{},
-        SampleBuffer{},
-        BufferLength{},
+        sample_buffer{},
+        buffer_length{},
         offset{},
         lfo_range{},
         lfo_scale{},
         lfo_disp{},
-        Gain{},
+        gain{},
         waveform{},
         delay{},
         depth{},
@@ -56,15 +56,15 @@ public:
     }
 
 
-    ALfloat *SampleBuffer[2];
-    ALsizei BufferLength;
+    ALfloat *sample_buffer[2];
+    ALsizei buffer_length;
     ALsizei offset;
     ALsizei lfo_range;
     ALfloat lfo_scale;
     ALint lfo_disp;
 
     // Gains for left and right sides
-    ALfloat Gain[2][MAX_OUTPUT_CHANNELS];
+    ALfloat gain[2][MAX_OUTPUT_CHANNELS];
 
     // effect parameters
     ChorusWaveForm waveform;
@@ -87,10 +87,10 @@ protected:
         const union ALeffectProps *props) final;
 
     void do_process(
-        ALsizei samplesToDo,
-        const ALfloat(*samplesIn)[BUFFERSIZE],
-        ALfloat(*samplesOut)[BUFFERSIZE],
-        ALsizei numChannels) final;
+        ALsizei sample_count,
+        const ALfloat(*src_samples)[BUFFERSIZE],
+        ALfloat(*dst_samples)[BUFFERSIZE],
+        ALsizei channel_count) final;
 }; // ChorusEffect
 
 
@@ -121,9 +121,9 @@ static void GetSinusoidDelays(ALint *delays, ALsizei offset, const ALsizei lfo_r
 
 void ChorusEffect::do_construct()
 {
-    BufferLength = 0;
-    SampleBuffer[0] = nullptr;
-    SampleBuffer[1] = nullptr;
+    buffer_length = 0;
+    sample_buffer[0] = nullptr;
+    sample_buffer[1] = nullptr;
     offset = 0;
     lfo_range = 1;
     waveform = CWF_Triangle;
@@ -131,9 +131,9 @@ void ChorusEffect::do_construct()
 
 void ChorusEffect::do_destruct()
 {
-    al_free(SampleBuffer[0]);
-    SampleBuffer[0] = nullptr;
-    SampleBuffer[1] = nullptr;
+    al_free(sample_buffer[0]);
+    sample_buffer[0] = nullptr;
+    sample_buffer[1] = nullptr;
 }
 
 ALboolean ChorusEffect::do_update_device(
@@ -145,22 +145,22 @@ ALboolean ChorusEffect::do_update_device(
     maxlen = fastf2i(AL_CHORUS_MAX_DELAY * 2.0f * device->frequency) + 1;
     maxlen = NextPowerOf2(maxlen);
 
-    if (maxlen != BufferLength)
+    if (maxlen != buffer_length)
     {
         void *temp = al_calloc(16, maxlen * sizeof(ALfloat) * 2);
         if (!temp) return AL_FALSE;
 
-        al_free(SampleBuffer[0]);
-        SampleBuffer[0] = static_cast<ALfloat*>(temp);
-        SampleBuffer[1] = SampleBuffer[0] + maxlen;
+        al_free(sample_buffer[0]);
+        sample_buffer[0] = static_cast<ALfloat*>(temp);
+        sample_buffer[1] = sample_buffer[0] + maxlen;
 
-        BufferLength = maxlen;
+        buffer_length = maxlen;
     }
 
-    for (it = 0; it < BufferLength; it++)
+    for (it = 0; it < buffer_length; it++)
     {
-        SampleBuffer[0][it] = 0.0f;
-        SampleBuffer[1][it] = 0.0f;
+        sample_buffer[0][it] = 0.0f;
+        sample_buffer[1][it] = 0.0f;
     }
 
     return AL_TRUE;
@@ -192,9 +192,9 @@ void ChorusEffect::do_update(
 
     /* Gains for left and right sides */
     CalcAngleCoeffs(-F_PI_2, 0.0f, 0.0f, coeffs);
-    ComputePanningGains(device->dry, coeffs, 1.0F, Gain[0]);
+    ComputePanningGains(device->dry, coeffs, 1.0F, gain[0]);
     CalcAngleCoeffs(F_PI_2, 0.0f, 0.0f, coeffs);
-    ComputePanningGains(device->dry, coeffs, 1.0F, Gain[1]);
+    ComputePanningGains(device->dry, coeffs, 1.0F, gain[1]);
 
     phase = props->chorus.phase;
     rate = props->chorus.rate;
@@ -227,20 +227,20 @@ void ChorusEffect::do_update(
 }
 
 void ChorusEffect::do_process(
-    ALsizei SamplesToDo,
-    const ALfloat(*SamplesIn)[BUFFERSIZE],
-    ALfloat(*SamplesOut)[BUFFERSIZE],
-    ALsizei NumChannels)
+    ALsizei sample_count,
+    const ALfloat(*src_samples)[BUFFERSIZE],
+    ALfloat(*dst_samples)[BUFFERSIZE],
+    ALsizei channel_count)
 {
-    ALfloat *leftbuf = SampleBuffer[0];
-    ALfloat *rightbuf = SampleBuffer[1];
-    const ALsizei bufmask = BufferLength - 1;
+    ALfloat *leftbuf = sample_buffer[0];
+    ALfloat *rightbuf = sample_buffer[1];
+    const ALsizei bufmask = buffer_length - 1;
     ALsizei i, c;
     ALsizei base;
 
-    for (base = 0; base < SamplesToDo;)
+    for (base = 0; base < sample_count;)
     {
-        const ALsizei todo = mini(128, SamplesToDo - base);
+        const ALsizei todo = mini(128, sample_count - base);
         ALfloat temps[128][2];
         ALint moddelays[2][128];
 
@@ -264,31 +264,31 @@ void ChorusEffect::do_process(
 
         for (i = 0; i < todo; i++)
         {
-            leftbuf[offset&bufmask] = SamplesIn[0][base + i];
+            leftbuf[offset&bufmask] = src_samples[0][base + i];
             temps[i][0] = leftbuf[(offset - moddelays[0][i])&bufmask] * feedback;
             leftbuf[offset&bufmask] += temps[i][0];
 
-            rightbuf[offset&bufmask] = SamplesIn[0][base + i];
+            rightbuf[offset&bufmask] = src_samples[0][base + i];
             temps[i][1] = rightbuf[(offset - moddelays[1][i])&bufmask] * feedback;
             rightbuf[offset&bufmask] += temps[i][1];
 
             offset++;
         }
 
-        for (c = 0; c < NumChannels; c++)
+        for (c = 0; c < channel_count; c++)
         {
-            ALfloat gain = Gain[0][c];
-            if (fabsf(gain) > GAIN_SILENCE_THRESHOLD)
+            ALfloat channel_gain = gain[0][c];
+            if (fabsf(channel_gain) > GAIN_SILENCE_THRESHOLD)
             {
                 for (i = 0; i < todo; i++)
-                    SamplesOut[c][i + base] += temps[i][0] * gain;
+                    dst_samples[c][i + base] += temps[i][0] * channel_gain;
             }
 
-            gain = Gain[1][c];
-            if (fabsf(gain) > GAIN_SILENCE_THRESHOLD)
+            channel_gain = gain[1][c];
+            if (fabsf(channel_gain) > GAIN_SILENCE_THRESHOLD)
             {
                 for (i = 0; i < todo; i++)
-                    SamplesOut[c][i + base] += temps[i][1] * gain;
+                    dst_samples[c][i + base] += temps[i][1] * channel_gain;
             }
         }
 
