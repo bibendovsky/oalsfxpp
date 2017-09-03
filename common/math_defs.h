47a83d4687ca1ed806f4b0f3f4a4628658994240
bibendovsky <bibendovsky@hotmail.com>
2017-09-03 16:30:13 +0300

    Convert DEG2RAD to function

--- common/math_defs.h
+++ common/math_defs.h
@@ -6,8 +6,12 @@ constexpr auto F_PI = 3.14159265358979323846F;
 constexpr auto F_PI_2 = 1.57079632679489661923F;
 constexpr auto F_TAU = 6.28318530717958647692F;
 
-#define DEG2RAD(x)  ((float)(x) * (F_PI/180.0f))
-#define RAD2DEG(x)  ((float)(x) * (180.0f/F_PI))
+
+constexpr float DEG2RAD(
+    const float x)
+{
+    return x * (F_PI / 180.0F);
+}
 
 
 #endif /* AL_MATH_DEFS_H */
