8bd57728a3e7a2c651c8863f1d763fc83c81e592
bibendovsky <bibendovsky@hotmail.com>
2017-09-12 18:22:05 +0300

    Merge structs

--- OpenAL32/Include/alSource.h
+++ OpenAL32/Include/alSource.h
@@ -8,30 +8,23 @@
 
 struct ALsource
 {
-    // Direct filter and auxiliary send info.
-
-    struct Direct
+    struct Base
     {
         float gain_;
         float gain_hf_;
         float hf_reference_;
         float gain_lf_;
         float lf_reference_;
-    }; // Direct
+    }; // Base
 
-    struct Send
+    struct Send : Base
     {
         EffectSlot* effect_slot_;
-        float gain_;
-        float gain_hf_;
-        float hf_reference_;
-        float gain_lf_;
-        float lf_reference_;
     }; // Send
 
     using SendUPtr = std::unique_ptr<Send>;
 
-    Direct direct_;
+    Base direct_;
     SendUPtr send_;
 
     // Source state (initial, playing, paused, or stopped)
