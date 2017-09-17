11a2aa52d58abec797f4e305b07d5fe1ddd5cb29
bibendovsky <bibendovsky@hotmail.com>
2017-09-17 20:16:12 +0300

    Remove all OpenAL files

--- common/align.h
+++
@@ -1,8 +0,0 @@
-#ifndef AL_ALIGN_H
-#define AL_ALIGN_H
-
-#ifndef alignas
-#define alignas(_x) ALIGN(_x)
-#endif
-
-#endif /* AL_ALIGN_H */
--- common/bool.h
+++
@@ -1,6 +0,0 @@
-#ifndef AL_BOOL_H
-#define AL_BOOL_H
-
-#include <stdbool.h>
-
-#endif /* AL_BOOL_H */
